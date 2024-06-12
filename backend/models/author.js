7d4af76ab598192a187e435ffdb27d3b47c4a63a
Mohammed Ajiach <ajiach.mohammed@ensi.ma>
2024-06-12 10:47:54 +0100

    Initial commit

---
+++ backend/models/author.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Author =mongoose.model('Author',{
+
+    name:{
+        type:String
+    }, 
+    email: {
+    	type:String,
+        unique:true        
+    },
+    password: {
+        type:String
+    },
+    about:{
+        type:String
+    },
+    image: {
+        type:String
+    }
+
+});
+
+module.exports = Author;
